d926df094ecd4a3513899944be57fca6fd137f7f
ColbyDomingos <colbydomingos@my.whccd.edu>
2024-11-29 15:37:28 -0800

    finished all questions

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("What worked well was learning basic syntax and concepts in JS. The reading was also very helpful when discovering about certain topics."); 
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I would change the course to be faster paced and learning a lot more industry ready skills.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I did learn mostly what I expected, some topics I felt weren't covered to the depth I would like but overall a very well put together course.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("I was fairly new to all of JS, I didn't know how to use functions very well and adding items from the html pages were new to me as well.");
